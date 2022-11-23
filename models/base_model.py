aa9a24e52c157b6eac86bd20544673678a3799f7
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-11-23 18:28:01 -0300

    checking2

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,10 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, INTEGER, String, DateTime
-​
-​
+
+
 Base = declarative_base()
-​
+
 class BaseModel:
     """A base class for all hbnb models"""
 
@@ -23,19 +23,19 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         self.save()
-​
+
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
         return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
-​
+
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
-​
+
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
@@ -47,7 +47,7 @@ class BaseModel:
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
-​
+
     def delete(self):
         """
             delete the current instance from the storage
