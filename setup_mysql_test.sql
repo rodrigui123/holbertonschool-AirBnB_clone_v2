e3c8682f211c251be5dbb4257d083144248aa949
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-11-24 14:17:07 -0300

    task4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,6 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE hbnb_test;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
