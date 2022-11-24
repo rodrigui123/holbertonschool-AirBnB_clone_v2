44b2c7adc385bf4c40ee8435c5cd0309b7a779bd
gasstin <gastonl1994@gmail.com>
2022-11-24 14:14:08 +0000

    adding task 1 - adding test_console

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,7 +13,7 @@ class test_console(unittest.TestCase):
     """ Class to test the console"""
     
     
-    def test_create(self):
+    def test_emptyline(self):
         console_test = console.HBNBCommand
         
         self.assertTrue(console_test("\n"), 'pass')
