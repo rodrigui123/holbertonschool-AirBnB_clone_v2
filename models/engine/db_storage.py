9fc29e6cc474317d55b9ac42b63df5f35c710c15
gasstin <gastonl1994@gmail.com>
2022-11-24 18:07:51 +0000

    adding task 5 - adding db_storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+"""This module defines a class to manage data base storage for hbnb clone"""
+import json
+from os import getenv
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base, BaseModel
+
+
+class DBStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __engine = None
+    __session = None
+    
+    
+    def __init__(self):
+        self.__engine = create_engine(f"mysql+mysqldb://{getenv('HBNB_MYSQL_USER')}:{getenv('HBNB_MYSQL_PWD')}@{getenv('HBNB_MYSQL_HOST')}:3306/{getenv('HBNB_MYSQL_DB')}", pool_pre_ping=True)
+        
+        if getenv('HBNB_ENV') == 'test': # drop all tables
+            MetaData.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'State': State, 'User': User, 'Place': Place,
+                    'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+
+        final_dictionary = {}
+        if cls:
+            for object in self.__session.query(classes[str(cls)]):
+                key_auxiliar = str(cls) + '.' + object.id
+                final_dictionary[key_auxiliar] = object.to_dict()
+        else:
+            print("3333333333333333333333333")
+            for class_aux in classes.keys():
+                print("444444444444444444444444444444444444")
+                for object in self.__session.query(classes[class_aux]):
+                    print("555555555555555555555555555")
+                    key_auxiliar = str(class_aux) + '.' + object.id
+                    final_dictionary[key_auxiliar] = object.to_dict()
+                    print(final_dictionary)
+        return final_dictionary
+
+    def new(self, obj):
+        """
+            add the object to the current database session
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+            commit all changes of the current database session
+        """
+        self.__session.commit()
+    
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        Base.metadata.create_all(self.__engine)
+        
+        Sess = scoped_session(sessionmaker(expire_on_commit=False, bind=self.__engine))
+        self.__session = Sess()
+
+        
+    def delete(self, obj=None):
+        """
+            delete from the current database session obj if not None
+        """
+        if obj:
+            self.__session.delete(obj)
