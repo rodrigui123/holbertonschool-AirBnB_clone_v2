c905b5e63fed9177b9cef0db3c8412da3e18a254
gasstin <gastonl1994@gmail.com>
2022-11-24 13:58:28 +0000

    adding task 1 - adding test_console

---
+++ tests/test_console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+import unittest
+from models.base_model import BaseModel
+from models import storage
+import console
+from unittest.mock import patch
+from io import StringIO
+import os
+
+
+class test_console(unittest.TestCase):
+    """ Class to test the console"""
+    
+    
+    def test_create(self):
+        console_test = console.HBNBCommand
+        
+        self.assertTrue(console_test("\n"), 'pass')
+        
+    
