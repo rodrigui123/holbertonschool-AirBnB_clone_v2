3b320bf60d9f816f10b8073c53769af42be3abe4
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-11-27 22:39:42 -0300

    tests

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module for testing file storage"""
+
+
+import unittest
+
+
+class test_db_storage(unittest.TestCase):
+    """ Class to test the file storage method """
+
+    def test_dbstorage(self):
+        self.assertEqual(1, 1)
