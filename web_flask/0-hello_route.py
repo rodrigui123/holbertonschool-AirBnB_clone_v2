63ac1cd7031b0de31d30b9164782773f8b48bf7d
Rodrigo Olivera <rodrigolivera@Rodrigos-MacBook-Air.local>
2022-12-01 14:28:00 -0300

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application"""
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,6 +7,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
